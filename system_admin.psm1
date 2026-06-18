bb2443b4aa9d7dbbf4105c2bf3496c70cf5e81b6
Veasna <veasna.rong@student.tafesa.edu.au>
2026-06-18 12:55:39 +0930

    Comments only - No code implementation yet

---
+++ system_admin.psm1
@@ -0,0 +1,44 @@
+﻿<#
+===========================================================================
+Assessment2: Operating System Information Configuration
+Author: Veasna Rong
+Script Name: system_admin.psm1
+
+Virtual Machines:
+
+    Windows Server 2022: Server1
+    ip address: 192.168.1.2 255.255.255.0
+    NIC: VMnet1 (Host-Only)
+
+    Windows 11: W11Client1
+    ip address: 192.168.1.102 255.255.255.0
+    NIC: VMnet1 (Host-Only)
+
+* note: Both VMs Firewalls are disabled *
+
+VMware network setting configured:
+    Network Type: Host-Only VMnet1
+    Subnet IP address: 192.168.1.0
+    Subnet Mask: 255.255.255.0
+=========================================================================== 
+#>
+
+# Task 1 – Logging Function and Test Function
+
+# Task 2 – Promote Server to Domain Controller
+
+# Task 3 – Connect to Domain Computer
+
+# Task 4a – Create OUs from CSV
+
+# Task 4b – Create Users from CSV
+
+# Task 5 – Join Computer to Domain
+
+# Task 6 – Configure DHCP Scope
+
+# Task 7 – Top Ten System Errors
+
+# Task 8 – Schedule Disk Cleanup
+
+# Task 9 – Drive Mapping Script
